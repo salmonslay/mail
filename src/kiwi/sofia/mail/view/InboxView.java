496674268d817392b07052cedd097fa4ccbf7bec
Sofia Lindgren <hi@sofia.kiwi>
2024-05-02 15:51:45 +0200

    Wrap long bodies

--- src/kiwi/sofia/mail/view/InboxView.java
+++ src/kiwi/sofia/mail/view/InboxView.java
@@ -28,6 +28,7 @@ public class InboxView implements SofView {
 
             messageObservableList.add("a");
             messageObservableList.add("hello hello");
+            messageObservableList.add("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum");
 
             contentPane.getChildren().add(loader.getRoot());
         } catch (Exception e) {
