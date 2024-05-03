fb90f89d3d25e4f04c14f453f606c9984ece1758
Sofia Lindgren <hi@sofia.kiwi>
2024-05-03 11:40:50 +0200

    Allow skipping login

--- src/kiwi/sofia/mail/Main.java
+++ src/kiwi/sofia/mail/Main.java
@@ -9,7 +9,13 @@ import kiwi.sofia.mail.view.LoginView;
 import kiwi.sofia.mail.view.SofView;
 
 public class Main extends Application {
+    public static boolean skipLogin = false;
+
     public static void main(String[] args) {
+        if (args.length > 0 && args[0].equals("--skip-login")) {
+            skipLogin = true;
+        }
+
         launch(args);
 
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
@@ -21,7 +27,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) {
-        SofView view = new InboxView();
+        SofView view = skipLogin ? new InboxView() : new LoginView();
         Pane root = view.getView();
         Scene scene = new Scene(root, 800, 600);
         stage.setTitle("Mail");
