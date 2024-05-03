74a4f47d6017cac4f7d79638b2c2a8f4b9f3a351
Sofia Lindgren <hi@sofia.kiwi>
2024-05-03 13:35:36 +0200

    Add longer default body

--- src/kiwi/sofia/mail/template/EmailCell.java
+++ src/kiwi/sofia/mail/template/EmailCell.java
@@ -46,7 +46,7 @@ public class EmailCell extends ListCell<Message> {
             fromLabel.setText(message.getFrom()[0].toString());
 
             dateLabel.setText(message.getSentDate().toString());
-            bodyLabel.setText("body");
+            bodyLabel.setText("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum");
             setGraphic(gridPane);
             setText(null);
         } catch (Exception e) {
