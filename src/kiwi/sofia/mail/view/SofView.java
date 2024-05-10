8c38ad077923f99b2507e6d10118d3ff847844b7
Sofia Lindgren <hi@sofia.kiwi>
2024-05-10 08:39:43 +0200

    Extend initializable in sofview

--- src/kiwi/sofia/mail/view/SofView.java
+++ src/kiwi/sofia/mail/view/SofView.java
@@ -1,10 +1,18 @@
 package kiwi.sofia.mail.view;
 
+import javafx.fxml.Initializable;
 import javafx.scene.layout.Pane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
 
 /**
  * Interface for all views in the program.
  */
-public interface SofView {
+public interface SofView extends Initializable {
     Pane getView();
+
+    @Override
+    default void initialize(URL location, ResourceBundle resources) {
+    }
 }
