ab78ca1e8b5d3a5ceb9036c6c8c1e1845666e091
Sofia Lindgren <hi@sofia.kiwi>
2024-05-14 16:15:58 +0200

    Add attachments saving

--- src/kiwi/sofia/mail/common/BodyParser.java
+++ src/kiwi/sofia/mail/common/BodyParser.java
@@ -3,8 +3,10 @@ package kiwi.sofia.mail.common;
 import jakarta.mail.BodyPart;
 import jakarta.mail.MessagingException;
 import jakarta.mail.Multipart;
+import jakarta.mail.internet.MimeBodyPart;
 import org.jsoup.Jsoup;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class BodyParser {
@@ -54,6 +56,49 @@ public class BodyParser {
             }
         } else {
             return "<h1>Failed to load email body</h1>Type " + body.getClass().getName() + " is not supported.";
+        }
+    }
+
+    /**
+     * Travels through the body of an email and saves any attachments to the specified path.
+     *
+     * @param body The body of the email.
+     * @param path The path to save the attachments to.
+     */
+    public static void saveAttachments(Object body, String path) {
+        if (!(body instanceof Multipart))
+            return;
+
+        try {
+            Multipart multipart = (Multipart) body;
+            for (int i = 0; i < multipart.getCount(); i++) {
+                BodyPart part = multipart.getBodyPart(i);
+                String contentType = part.getContentType().toLowerCase();
+                if (contentType.contains("multipart/alternative") || contentType.contains("multipart/related")) {
+                    saveAttachments(part.getContent(), path);
+                } else if (contentType.contains("name=")) {
+                    System.out.printf("Saving attachment %s\n", part.getFileName());
+                    saveFile(part, part.getFileName(), path);
+                }
+            }
+        } catch (MessagingException | IOException e) {
+            System.out.println("Failed to save attachments: " + e.getMessage());
+        }
+    }
+
+    /**
+     * Saves an attachment to the specified path.
+     *
+     * @param part The attachment to save.
+     * @param name The name of the attachment.
+     * @param path The path to save the attachment to.
+     */
+    private static void saveFile(BodyPart part, String name, String path) {
+        try {
+            ((MimeBodyPart) part).saveFile(path + "/" + name);
+            System.out.printf("Saved attachment %s\n", name);
+        } catch (IOException | MessagingException e) {
+            System.out.println("Failed to save attachment: " + e.getMessage());
         }
     }
 
--- src/kiwi/sofia/mail/view/EmailView.java
+++ src/kiwi/sofia/mail/view/EmailView.java
@@ -3,17 +3,28 @@ package kiwi.sofia.mail.view;
 import jakarta.mail.Message;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.web.WebView;
+import javafx.stage.DirectoryChooser;
 import kiwi.sofia.mail.common.BodyParser;
+import kiwi.sofia.mail.template.EmailCell;
+
+import java.io.File;
+import java.util.prefs.Preferences;
 
 public class EmailView implements SofView {
     private final Pane rootPane;
     private final Message message;
+    private Object content;
 
-
+    @FXML
+    private Button replyAllButton;
+    @FXML
+    private Button attachmentsButton;
     @FXML
     private WebView webView;
     @FXML
@@ -29,7 +40,8 @@ public class EmailView implements SofView {
             loader.load();
 
             subjectLabel.setText(message.getSubject());
-            setBody(message.getContent());
+            content = message.getContent();
+            setBody(content);
 
             rootPane.getChildren().add(loader.getRoot());
         } catch (Exception e) {
@@ -47,7 +59,41 @@ public class EmailView implements SofView {
     }
 
     @FXML
-    public void goBackToInbox(){
+    public void actionGoBackToInbox() {
         ClientView.showInbox();
+    }
+
+    @FXML
+    public void actionReply() {
+        System.out.println("Replying to email");
+    }
+
+    @FXML
+    public void actionForward() {
+        System.out.println("Forwarding email");
+    }
+
+    @FXML
+    public void actionReplyAll() {
+        System.out.println("Replying to all");
+    }
+
+    @FXML
+    public void actionDownloadAttachments() {
+        Preferences prefs = Preferences.userNodeForPackage(EmailView.class);
+        String lastPath = prefs.get("lastPath", System.getProperty("user.home"));
+
+        DirectoryChooser directoryChooser = new DirectoryChooser();
+        directoryChooser.setInitialDirectory(new java.io.File(lastPath));
+        directoryChooser.setTitle("Select directory to save attachments");
+        File window = directoryChooser.showDialog(rootPane.getScene().getWindow());
+
+        if (window == null)
+            return;
+
+        String path = window.getAbsolutePath();
+        prefs.put("lastPath", path);
+
+        BodyParser.saveAttachments(content, path);
     }
 }
