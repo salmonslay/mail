6012f6163e48eecbe53798bf08989ad61cf18c7e
Sofia Lindgren <hi@sofia.kiwi>
2024-05-15 14:50:09 +0200

    Add button for sending and attaching files

--- src/kiwi/sofia/mail/view/AuthorView.java
+++ src/kiwi/sofia/mail/view/AuthorView.java
@@ -2,6 +2,7 @@ package kiwi.sofia.mail.view;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 import kiwi.sofia.mail.template.EmailCell;
 
@@ -9,11 +10,16 @@ public class AuthorView implements SofView {
     @FXML
     private Pane rootPane;
 
+    @FXML
+    private Label filesAttachedLabel;
+
     private AuthorView() {
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/AuthorView.fxml"));
             loader.setController(this);
             loader.load();
+
+            filesAttachedLabel.setText("No files attached");
 
         } catch (Exception e) {
             System.out.println("Failed to load AuthorView.fxml" + e.getMessage());
@@ -37,5 +43,15 @@ public class AuthorView implements SofView {
     @FXML
     public void actionBackToInbox() {
         ClientView.showInbox();
+    }
+
+    @FXML
+    public void actionAttachFiles() {
+        System.out.println("Attaching file");
+    }
+
+    @FXML
+    public void actionSend() {
+        System.out.println("Sending email");
     }
 }
