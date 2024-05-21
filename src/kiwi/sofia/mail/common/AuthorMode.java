a3c009b455ab8b38863d424dd1e36a2b9f5ae9d7
Sofia Lindgren <hi@sofia.kiwi>
2024-05-21 13:21:50 +0200

    Explain different author modes

--- src/kiwi/sofia/mail/common/AuthorMode.java
+++ src/kiwi/sofia/mail/common/AuthorMode.java
@@ -1,5 +1,23 @@
 package kiwi.sofia.mail.common;
 
 public enum AuthorMode {
-    NEW, REPLY, REPLY_ALL, FORWARD
+    /**
+     * This is a new email being composed and all fields should be empty by default.
+     */
+    NEW,
+
+    /**
+     * The user is replying to an email, and the email fields should be pre-filled with the original email's data.
+     */
+    REPLY,
+
+    /**
+     * The user is replying to all recipients, and the email fields should be pre-filled with the original email's data.
+     */
+    REPLY_ALL,
+
+    /**
+     * The user is forwarding an email, and the content and subject should be pre-filled, but not recipient-field.
+     */
+    FORWARD
 }
