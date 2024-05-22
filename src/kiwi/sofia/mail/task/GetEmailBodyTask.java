a0d3bb97fddcec8aad3a74c2413d2d25236181f9
Sofia Lindgren <hi@sofia.kiwi>
2024-05-22 13:38:41 +0200

    Mark messages as seen once opened

--- src/kiwi/sofia/mail/task/GetEmailBodyTask.java
+++ src/kiwi/sofia/mail/task/GetEmailBodyTask.java
@@ -1,12 +1,13 @@
 package kiwi.sofia.mail.task;
 
+import jakarta.mail.Flags;
 import jakarta.mail.Message;
 import javafx.concurrent.Task;
 import kiwi.sofia.mail.common.BodyParser;
 import kiwi.sofia.mail.common.Pair;
 
 /**
- * Basic task to set the content of an email.
+ * Basic task to set the content of an email, and marks it as read.
  */
 public class GetEmailBodyTask extends Task<Pair<String, String>> {
     private final Message msg;
@@ -17,6 +18,8 @@ public class GetEmailBodyTask extends Task<Pair<String, String>> {
 
     @Override
     protected Pair<String, String> call() throws Exception {
+        msg.setFlag(Flags.Flag.SEEN, true);
+
         Object body = msg.getContent();
         return BodyParser.parse(body, true);
     }
