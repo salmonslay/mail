c77b84592e48c539ea6cebf3e793cdefbc1ce080
Sofia Lindgren <hi@sofia.kiwi>
2024-05-23 11:35:52 +0200

    Clear credentials at startup

--- src/kiwi/sofia/mail/Main.java
+++ src/kiwi/sofia/mail/Main.java
@@ -13,6 +13,8 @@ public class Main extends Application {
     public static boolean skipLogin = false;
 
     public static void main(String[] args) {
+        LoginView.clearCredentials(); // Clear credentials on startup in case of crash
+
         if (args.length > 0 && args[0].equals("--skip-login")) {
             skipLogin = true;
         }
