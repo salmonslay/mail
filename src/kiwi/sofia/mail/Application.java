dcd702171605fc35d43b250947ed4759ce669c3a
Sofia Lindgren <hi@sofia.kiwi>
2024-05-24 15:15:33 +0200

    Add build steps

---
+++ src/kiwi/sofia/mail/Application.java
@@ -0,0 +1,50 @@
+package kiwi.sofia.mail;
+
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import kiwi.sofia.mail.common.Constants;
+import kiwi.sofia.mail.view.ClientView;
+import kiwi.sofia.mail.view.LoginView;
+import kiwi.sofia.mail.view.SofView;
+
+public class Application extends javafx.application.Application {
+    public static boolean skipLogin = false;
+
+    public static void run(String[] args) {
+        LoginView.clearCredentials(); // Clear credentials on startup in case of crash
+
+        if (args.length > 0 && args[0].equals("--skip-login")) {
+            skipLogin = true;
+        }
+
+        launch(args);
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            System.out.println("Clearing credentials...");
+            LoginView.clearCredentials();
+            System.out.println("Credentials cleared");
+        }));
+    }
+
+    @Override
+    public void start(Stage stage) {
+        SofView view = skipLogin ? ClientView.getInstance() : new LoginView();
+        Pane root = view.getView();
+        Scene scene = new Scene(root, 1280, 720);
+        stage.setTitle(Constants.appName);
+        stage.getIcons().add(new Image("/favicon.png"));
+        stage.setResizable(false);
+        stage.setScene(scene);
+        stage.show();
+
+        stage.widthProperty().addListener((obs, oldVal, newVal) -> {
+            root.setPrefWidth((double) newVal);
+        });
+
+        stage.heightProperty().addListener((obs, oldVal, newVal) -> {
+            root.setPrefHeight((double) newVal);
+        });
+    }
+}
--- src/kiwi/sofia/mail/Main.java
+++ src/kiwi/sofia/mail/Main.java
@@ -1,51 +1,14 @@
 package kiwi.sofia.mail;
 
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.layout.Pane;
-import javafx.stage.Stage;
-import kiwi.sofia.mail.common.Constants;
-import kiwi.sofia.mail.view.ClientView;
-import kiwi.sofia.mail.view.LoginView;
-import kiwi.sofia.mail.view.SofView;
-
-public class Main extends Application {
+public class Main {
     public static boolean skipLogin = false;
 
+    /**
+     * Starter method. Used for built applications.
+     *
+     * @see <a href="https://stackoverflow.com/a/58498686/11420970">StackOverflow</a>
+     */
     public static void main(String[] args) {
-        LoginView.clearCredentials(); // Clear credentials on startup in case of crash
-
-        if (args.length > 0 && args[0].equals("--skip-login")) {
-            skipLogin = true;
-        }
-
-        launch(args);
-
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            System.out.println("Clearing credentials...");
-            LoginView.clearCredentials();
-            System.out.println("Credentials cleared");
-        }));
-    }
-
-    @Override
-    public void start(Stage stage) {
-        SofView view = skipLogin ? ClientView.getInstance() : new LoginView();
-        Pane root = view.getView();
-        Scene scene = new Scene(root, 1280, 720);
-        stage.setTitle(Constants.appName);
-        stage.getIcons().add(new Image("/favicon.png"));
-        stage.setResizable(false);
-        stage.setScene(scene);
-        stage.show();
-
-        stage.widthProperty().addListener((obs, oldVal, newVal) -> {
-            root.setPrefWidth((double) newVal);
-        });
-
-        stage.heightProperty().addListener((obs, oldVal, newVal) -> {
-            root.setPrefHeight((double) newVal);
-        });
+        Application.run(args);
     }
 }
